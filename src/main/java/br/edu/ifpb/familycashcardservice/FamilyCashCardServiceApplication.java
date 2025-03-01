c5fc585b6c18b2ecfa076af5f2b5c86286ceeca7
https_Luan_Fernandes <jose.luan@academico.ifpb.edu.br>
2025-03-01 18:18:45 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/familycashcardservice/FamilyCashCardServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.familycashcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyCashCardServiceApplication.class, args);
+    }
+
+}
